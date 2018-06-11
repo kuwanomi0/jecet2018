7773a0f753971ef2e33e2eef073e9ae62bd31289
kuwanomi0 <kuwanomi0@live.jp>
2018-06-11 16:49:00 +0900

    距離があっていなかったのでタイヤの直径修正

--- sample/Distance.cpp
+++ sample/Distance.cpp
@@ -9,8 +9,8 @@
 #include "Distance.h"
 
 int Distance::distanceAll(int left, int right) {
-    float leftdistance = 90.0 * PAI * left / 360;
-    float rightdistance = 90.0 * PAI * right / 360;
+    float leftdistance = 101.2 * PAI * left / 360;
+    float rightdistance = 101.2 * PAI * right / 360;
     int result = (leftdistance + rightdistance) / 2;
 
     return result;
