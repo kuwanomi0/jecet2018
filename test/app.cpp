02f0315b49306d94dc0e6a6a029a4ba2b0327334
kuwanomi0 <kuwanomi0@gmail.com>
2018-07-13 19:18:30 +0900

    BT通信するだけのプログラムの作成

--- test/app.cpp
+++ test/app.cpp
@@ -11,18 +11,6 @@
 #include "ev3api.h"
 #include "app.h"
 
-#include "TouchSensor.h"
-#include "SonarSensor.h"
-#include "ColorSensor.h"
-#include "GyroSensor.h"
-#include "Motor.h"
-#include "Clock.h"
-
-#include "control/BalancerCpp.h"
-#include "control/PID.h"
-
-using namespace ev3api;
-
 #define DEBUG
 
 #ifdef DEBUG
@@ -32,137 +20,34 @@ using namespace ev3api;
 #endif
 
 /* Bluetooth */
-static int32_t   bt_cmd = 0;      /* Bluetoothコマンド 1:リモートスタート */
-static FILE     *bt = NULL;      /* Bluetoothファイルハンドル */
+static int32_t   bt_cmd = 0;
+static FILE     *bt = NULL;
 
-/* 下記のマクロは個体/環境に合わせて変更する必要があります */
-/* 走行に関するマクロ */
-#define GYRO_OFFSET           2  /* ジャイロセンサオフセット値(角速度0[deg/sec]時) */
-#define RGB_TARGET          365  /*中央の境界線のRGBセンサ合計値 */
-#define RGB_NULL              7  /* 何もないときのセンサの合計 */
-#define KLP                 0.6  /* LPF用係数*/
-
-/* 超音波センサーに関するマクロ */
-#define SONAR_ALERT_DISTANCE 20  /* 超音波センサによる障害物検知距離[cm] */
-
-/* 尻尾に関するマクロ */
-#define TAIL_ANGLE_STAND_UP   93 /* 完全停止時の角度[度] */
-#define TAIL_ANGLE_ROKET      93 /* ロケットダッシュ時の角度[度] */
-#define TAIL_ANGLE_DRIVE       3 /* バランス走行時の角度[度] */
-#define TAIL_ANGLE_STOP       87 /* 停止処理時の角度[度] */
-#define TAIL_ANGLE_FIX      -0.5 /* 尻尾のずれ修正用 */
-#define KP_TAIL             2.7F /* 尻尾用定数P */
-#define KI_TAIL            0.02F /* 尻尾用定数I */
-#define KD_TAIL            14.0F /* 尻尾用定数D */
-#define PWM_ABS_MAX           60 /* 完全停止用モータ制御PWM絶対最大値 */
-
-
-/* LCDフォントサイズ */
-#define CALIB_FONT (EV3_FONT_SMALL)
-#define CALIB_FONT_WIDTH (6/* magic number*/)
-#define CALIB_FONT_HEIGHT (8/* magic number*/)
-
-/* 関数プロトタイプ宣言 */
-static void tail_control(int32_t angle);
-
-/* オブジェクトへのポインタ定義 */
-TouchSensor*    touchSensor;
-SonarSensor*    sonarSensor;
-ColorSensor*    colorSensor;
-GyroSensor*     gyroSensor;
-Motor*          leftMotor;
-Motor*          rightMotor;
-Motor*          tailMotor;
-Clock*          clock;
-Clock*          clock_gate;
-
-/* インスタンスの生成 */
-Balancer balancer;
-PID pid_walk(      0,       0,       0); /* 走行用のPIDインスタンス */
-PID pid_tail(KP_TAIL, KI_TAIL, KD_TAIL); /* 尻尾用のPIDインスタンス */
-
-/* 走行距離 */
-static int8_t    forward;      /* 前後進命令 */
-static int8_t    turn;         /* 旋回命令 */
-static int8_t    pwm_L = 0, pwm_R = 0;
-static rgb_raw_t rgb_level;    /* カラーセンサーから取得した値を格納する構造体 */
-static int       forward_course = 50; //TODO :2 コース関連 だいぶ改善されました
-static uint16_t  rgb_total = RGB_TARGET;
-static uint16_t  rgb_before;
-
-/* メインタスク */
+//*****************************************************************************
+// 関数名 : main_task
+// 引数   : 拡張情報
+// 返り値 : なし
+// 概要   : メインタスク
+//*****************************************************************************
 void main_task(intptr_t unused)
 {
-
-    /* 各オブジェクトを生成・初期化する */
-    touchSensor = new TouchSensor(PORT_1);
-    sonarSensor = new SonarSensor(PORT_2);
-    colorSensor = new ColorSensor(PORT_3);
-    gyroSensor  = new GyroSensor(PORT_4);
-    leftMotor   = new Motor(PORT_C);
-    rightMotor  = new Motor(PORT_B);
-    tailMotor   = new Motor(PORT_A);
-    clock       = new Clock();
-    clock_gate  = new Clock();
-
-    /* LCD画面表示 */
-    char buf[64];
-    ev3_lcd_fill_rect(0, 0, EV3_LCD_WIDTH, EV3_LCD_HEIGHT, EV3_LCD_WHITE);
-    ev3_lcd_draw_string("EV3way-ET 16JZ", 0, CALIB_FONT_HEIGHT*1);
-
-    /* 尻尾モーターのリセット */
-    for(int i = 0; i < 500; i++){
-        tailMotor->setPWM(-3);
-        clock->wait(1);
-    }
-    tailMotor->reset();
-
-    /* Open Bluetooth file */
     bt = ev3_serial_open_file(EV3_SERIAL_BT);
     assert(bt != NULL);
 
-    /* Bluetooth通信タスクの起動 */
     act_tsk(BT_TASK);
 
-    ev3_led_set_color(LED_ORANGE); /* 初期化完了通知 */
-
-    /* スタート待機 */
-    double angle = (double)TAIL_ANGLE_STAND_UP;
-
-    while(1)
-    {
-        tail_control(angle); /* 完全停止用角度に制御、調整も可 */
-
-        /* デフォルコース */
-        if (touchSensor->isPressed() || bt_cmd == 1)
-        {
-            break; /* タッチセンサが押された */
+    bt_cmd = 0;
+    while (bt_cmd != 6) {
+        if (bt_cmd == 0) {
+            syslog(LOG_NOTICE, "TRUE\r");
         }
-
-        clock->sleep(10); /* 10msecウェイト */
+        else {
+            syslog(LOG_NOTICE, "FALSE\r");
+        }
+        tslp_tsk(100);
     }
 
-    /* 走行モーターエンコーダーリセット */
-    leftMotor->reset();
-    rightMotor->reset();
-
-    /* ジャイロセンサーリセット */
-    gyroSensor->reset();
-    balancer.init(GYRO_OFFSET); /* 倒立振子API初期化 */
-
-    ev3_led_set_color(LED_GREEN); /* スタート通知 */
-
-    ER er = ev3_sta_cyc(CYC_HANDLER);   //周期ハンドラを起動
-    sprintf(buf, "main_task: error_code=%d", MERCD(er) );   // APIのエラーコードの表示
-    //ev3_lcd_draw_string(buf, 0, CALIB_FONT_HEIGHT*1);     // の仕方です。
-
-    /* 自タスク(メインタスク）を待ち状態にする */
-    slp_tsk();
-
-    leftMotor->reset();
-    rightMotor->reset();
-    tailMotor->reset();
-
+    syslog(LOG_NOTICE, "END");
 
     ter_tsk(BT_TASK);
     fclose(bt);
@@ -178,60 +63,6 @@ void main_task(intptr_t unused)
 //*****************************************************************************
 void controller_task(intptr_t unused)
 {
-    /* 時間計測開始 */
-	// get_utm(&start_time);
-
-    int32_t motor_ang_l, motor_ang_r;
-    int32_t gyro, volt;
-
-    tail_control(TAIL_ANGLE_DRIVE); /* バランス走行用角度に制御 */
-
-    rgb_before = rgb_total; //LPF用前処理
-    colorSensor->getRawColor(rgb_level); /* RGB取得 */
-    rgb_total = (rgb_level.r + rgb_level.g + rgb_level.b)  * KLP + rgb_before * (1 - KLP); //LPF
-
-    /* バックボタン, 転倒時停止処理 */
-    if (bt_cmd == 6) {
-        ev3_led_set_color(LED_RED);
-        wup_tsk(MAIN_TASK);        //メインタスクを起床する
-        ev3_stp_cyc(CYC_HANDLER);  //周期ハンドラを停止する
-    }
-
-    forward = forward_course; /* 前進命令 */
-    turn = pid_walk.calcControl(RGB_TARGET - rgb_total);
-
-    /* 倒立振子制御API に渡すパラメータを取得する */
-    motor_ang_l = leftMotor->getCount();
-    motor_ang_r = rightMotor->getCount();
-    gyro = gyroSensor->getAnglerVelocity();
-    volt = ev3_battery_voltage_mV();
-
-    balancer.setCommand(forward, turn);
-    balancer.update(gyro, motor_ang_r, motor_ang_l, volt);
-    pwm_L = balancer.getPwmRight();
-    pwm_R = balancer.getPwmLeft();
-
-    /* EV3ではモーター停止時のブレーキ設定が事前にできないため */
-    /* 出力0時に、その都度設定する */
-    if (pwm_L == 0)
-    {
-         leftMotor->stop();
-    }
-    else
-    {
-        leftMotor->setPWM(pwm_L);
-    }
-
-    if (pwm_R == 0)
-    {
-         rightMotor->stop();
-    }
-    else
-    {
-        rightMotor->setPWM(pwm_R);
-    }
-
-    ext_tsk();
 }
 
 //*****************************************************************************
@@ -242,30 +73,7 @@ void controller_task(intptr_t unused)
 //*****************************************************************************
 void cyc_handler(intptr_t unused)
 {
-    // コントローラタスクを起動する
     act_tsk(CONTROLLER_TASK);
-}
-
-//*****************************************************************************
-// 関数名 : tail_control
-// 引数 : angle (モータ目標角度[度])
-// 返り値 : 無し
-// 概要 : 走行体完全停止用モータの角度制御
-//*****************************************************************************
-static void tail_control(int32_t angle)
-{
-    int pwm = (int)pid_tail.calcControl(angle - tailMotor->getCount() - TAIL_ANGLE_FIX); /* PID制御 */
-    /* PWM出力飽和処理 */
-    if (pwm > PWM_ABS_MAX)
-    {
-        pwm = PWM_ABS_MAX;
-    }
-    else if (pwm < -PWM_ABS_MAX)
-    {
-        pwm = -PWM_ABS_MAX;
-    }
-
-    tailMotor->setPWM(pwm);
 }
 
 //*****************************************************************************
@@ -279,7 +87,7 @@ void bt_task(intptr_t unused)
 {
     while(1)
     {
-        uint8_t c = fgetc(bt); /* 受信 */
+        uint8_t c = fgetc(bt);
         switch(c)
         {
         case '0':
@@ -297,5 +105,6 @@ void bt_task(intptr_t unused)
         default:
             break;
         }
+        fputc(c, bt); /* エコーバック */
     }
 }
